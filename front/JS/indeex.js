3abca87a9a05acf69f8ef450d8ea2fcc729e1955
dimadeloseros1 <dimadeloseros1@gmail.com>
2021-11-05 12:17:00 +0000

    sss

--- front/JS/indeex.js
+++ front/JS/indeex.js
@@ -1,29 +1,39 @@
-// Get all the information from Bilal's API
-const getItems = async() => {
-	try {
-		let response = await fetch("http://localhost:3000/api/products");
-		return await response.json();
-	} catch (error) {
-		console.log("Error : " + error);
-	}
-}
+const getAllProducts = () => {
+	fetch("http://localhost:3000/api/products")
+	.then(function (res) {
+		return res.json();
+	}).catch((error) => {
+		let items = document.querySelector('.items');
+		items.innerHTML = 
+		alert('error has been produced!')
+	})
 
-// Handle the render on the HTML
-(async function renderItems() {
-	let items = await getItems();
-	let htmlRender = "";
-	items.forEach((item) => {
-		let htmlContent = `
-		<a href="./product.html?id=${item._id}">
-			<article>
-				<img src="${item.imageUrl}" alt="${item.altTxt}">
-				<h3 class="productName">${item.name}</h3>
-				<p class="productDescription">${item.description}</p>
-			</article>
-		</a>
-		`;
-		htmlRender += htmlContent;
-	});
-	const itemContainer = document.getElementById("items");
-	itemContainer.innerHTML += htmlRender;
-})();
+	.then(async function (resultOfApi) {
+		const articles = resultOfApi;
+		console.log(articles);
+		for (let article in articles) {
+			let productLink = document.createElement("a");
+			document.querySelector(".items").appendChild(productLink);
+			productLink.href = `product.html?id=${resultOfApi[article]._id}`;
+
+			let productArticle = document.createElement("article");
+			productLink.appendChild(productArticle);
+
+			let productImg = document.createElement('img');
+			productArticle.appendChild(productImg);
+			productImg.src = resultOfApi[article].imageUrl;
+			productImg.alt = resultOfApi[article].altTxt;
+
+			let productName = document.createElement('h3');
+			productArticle.appendChild(productName);
+			productName.classList.add('productName');
+			productName.innerHTML = resultOfApi[article].name;
+
+			let productDescritpion = document.createElement("p");
+			productArticle.appendChild(productDescritpion);
+			productDescritpion.classList.add("productName");
+			productDescritpion.innerHTML = resultOfApi[article].description;
+		}
+	})
+}
+getAllProducts();
