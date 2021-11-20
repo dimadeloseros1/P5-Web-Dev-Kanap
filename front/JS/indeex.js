389e83afdb28c2d77aebc5fad8c4d124880191a2
dimadeloseros1 <dimadeloseros1@gmail.com>
2021-11-20 13:01:49 +0000

    product page

--- front/JS/indeex.js
+++ front/JS/indeex.js
@@ -1,39 +1,32 @@
-const getAllProducts = () => {
+const apiData = async () => {
 	fetch("http://localhost:3000/api/products")
-	.then(function (res) {
-		return res.json();
-	}).catch((error) => {
-		let items = document.querySelector('.items');
-		items.innerHTML = 
-		alert('error has been produced!')
-	})
-
-	.then(async function (resultOfApi) {
-		const articles = resultOfApi;
+	.then(res => res.json())
+	.then(apiResult => {
+		const articles = apiResult;
 		console.log(articles);
 		for (let article in articles) {
 			let productLink = document.createElement("a");
 			document.querySelector(".items").appendChild(productLink);
-			productLink.href = `product.html?id=${resultOfApi[article]._id}`;
+			productLink.href = `product.html?id=${apiResult[article]._id}`;
 
 			let productArticle = document.createElement("article");
 			productLink.appendChild(productArticle);
 
 			let productImg = document.createElement('img');
 			productArticle.appendChild(productImg);
-			productImg.src = resultOfApi[article].imageUrl;
-			productImg.alt = resultOfApi[article].altTxt;
+			productImg.src = apiResult[article].imageUrl;
+			productImg.alt = apiResult[article].altTxt;
 
-			let productName = document.createElement('h3');
+			let productName = document.createElement("h3");
 			productArticle.appendChild(productName);
-			productName.classList.add('productName');
-			productName.innerHTML = resultOfApi[article].name;
+			productName.classList.add("productName");
+			productName.innerHTML = apiResult[article].name;
 
-			let productDescritpion = document.createElement("p");
-			productArticle.appendChild(productDescritpion);
-			productDescritpion.classList.add("productName");
-			productDescritpion.innerHTML = resultOfApi[article].description;
+			let productP = document.createElement('p');
+			productArticle.appendChild(productP);
+			productP.classList.add('ProductP');
+			productP.innerHTML = apiResult[article].description;
 		}
 	})
 }
-getAllProducts();
+apiData();
